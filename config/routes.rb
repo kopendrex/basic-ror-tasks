085426e9df47f69a13800344beea6b886bdf889e
Karol Kania <44871508+karolkania@users.noreply.github.com>
2018-11-30 18:09:13 +0100

    Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :high_scores
+
+  # Sidekiq dashboard
+  require 'sidekiq/web'
+  mount Sidekiq::Web => "/sidekiq"
+
+  # Sidekiq trigger
+  get "/dummy" => "dummy#index", as: "dummy_index"
+  get "/dummy/report" => "dummy#report", as: "dummy_report"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
